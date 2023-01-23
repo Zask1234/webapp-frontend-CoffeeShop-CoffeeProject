ec1f26ee09ffc28be458281022e177cf8b4b2dd2
Zask1234 <kivenfallado123@gmail.com>
2023-01-23 19:21:06 +0800

    Adding Dashboard and users template

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/users', function () {
+    return view('users');
+})->middleware(['auth', 'verified'])->name('users');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -29,3 +33,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
